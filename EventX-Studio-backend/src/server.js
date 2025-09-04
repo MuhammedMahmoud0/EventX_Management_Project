2fb1b7349968d15f290abf8fc42d63d1d29c1e16
MuhammedMahmoud0 <muhammedmahmoud091@gmail.com>
2025-09-04 15:01:56 +0300

    update server.js

--- EventX-Studio-backend/src/server.js
+++ EventX-Studio-backend/src/server.js
@@ -16,29 +16,7 @@ const userRoutes = require("./routes/userRoutes");
 
 const app = express();
 
-// app.use(cors());
-
-// CORS setup
-const allowedOrigins = [
-    "http://localhost:5173",
-    "https://your-frontend-vercel-url.vercel.app",
-];
-
-app.use(
-    cors({
-        origin: function (origin, callback) {
-            if (!origin) return callback(null, true);
-            if (allowedOrigins.includes(origin)) return callback(null, true);
-            return callback(new Error("Not allowed by CORS"), false);
-        },
-        credentials: true,
-        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-        allowedHeaders: ["Content-Type", "Authorization"],
-    })
-);
-
-// Preflight
-app.options("*", cors());
+app.use(cors());
 app.use(helmet());
 app.use(morgan("dev"));
 app.use(express.json());
