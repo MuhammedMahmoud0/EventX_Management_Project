981775ead0da0f68886822b8a133f2d1e539275c
MuhammedMahmoud0 <muhammedmahmoud091@gmail.com>
2025-09-04 16:26:16 +0300

    update server.js

--- EventX-Studio-backend/src/server.js
+++ EventX-Studio-backend/src/server.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const express = require("express");
-const cors = require("cors");
+// const cors = require("cors");
 const helmet = require("helmet");
 const morgan = require("morgan");
 const connectDB = require("./config/db");
@@ -15,12 +15,12 @@ const seatMapRoutes = require("./routes/seatMapRoutes");
 const userRoutes = require("./routes/userRoutes");
 
 const app = express();
-app.use(
-    cors({
-        origin: "https://event-x-management-project.vercel.app/",
-    })
-);
-// app.use(cors());
+// app.use(
+//     cors({
+//         origin: "https://event-x-management-project.vercel.app/",
+//     })
+// );
+// // app.use(cors());
 app.use(helmet());
 app.use(morgan("dev"));
 app.use(express.json());
