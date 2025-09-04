fd04c8ca00e1f9a09ba5b2e5ed9f7ad9028464d8
MuhammedMahmoud0 <muhammedmahmoud091@gmail.com>
2025-09-04 16:45:28 +0300

    update vite.config.js

--- EventX-Studio-Frontend/vite.config.js
+++ EventX-Studio-Frontend/vite.config.js
@@ -1,28 +1,19 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 // // https://vite.dev/config/
 // export default defineConfig({
 //   plugins: [react(), tailwindcss()],
 // })
 
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
-
 export default defineConfig({
     plugins: [react(), tailwindcss()],
-    base: "/", // always serve static assets from root
-    build: {
-        outDir: "dist",
-        assetsDir: "assets",
-        sourcemap: false,
-    },
     server: {
         proxy: {
             "/api": {
-                target: "http://localhost:5000", // dev only
+                target: "https://event-x-management-project.vercel.app",
                 changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/api/, ""),
             },
         },
     },
