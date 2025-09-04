593c0899966769eca482c99bb894909344fbd52a
MuhammedMahmoud0 <muhammedmahmoud091@gmail.com>
2025-09-04 17:15:47 +0300

    update server.js

--- EventX-Studio-backend/src/server.js
+++ EventX-Studio-backend/src/server.js
@@ -20,7 +20,7 @@ app.use(
     cors({
         origin: [
             "http://localhost:5173",
-            "https://event-x-management-project-isy9.vercel.app",
+            "http://event-x-management-project-isy9.vercel.app",
         ],
         credentials: true,
         methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
