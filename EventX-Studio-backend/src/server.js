d9eaef5c94eb42f89d3e3d111063612d611c53cf
MuhammedMahmoud0 <muhammedmahmoud091@gmail.com>
2025-09-04 17:18:30 +0300

    update server.js

--- EventX-Studio-backend/src/server.js
+++ EventX-Studio-backend/src/server.js
@@ -21,6 +21,7 @@ app.use(
         origin: [
             "http://localhost:5173",
             "https://event-x-management-project-isy9.vercel.app",
+            "https://event-x-management-project-isy9-qqvyguln5.vercel.app",
         ],
         credentials: true,
         methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
